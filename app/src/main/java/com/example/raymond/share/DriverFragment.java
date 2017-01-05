8f786995f3a86ce3f58da5b0eb5ad4fda6a4b3a0
Raymond-95 <lauweiyan_95@hotmail.com>
2017-01-06 04:00:34 +0800

    AutoCompleteTextView done

--- app/src/main/java/com/example/raymond/share/DriverFragment.java
+++ app/src/main/java/com/example/raymond/share/DriverFragment.java
@@ -1,7 +1,9 @@
 package com.example.raymond.share;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +15,27 @@ import android.view.ViewGroup;
  */
 public class DriverFragment extends Fragment {
 
+    private FloatingActionButton fab;
 
     public DriverFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver, container, false);
-    }
 
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_driver, container, false);
+
+        fab = (FloatingActionButton) v.findViewById(R.id.addTrip);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Source.class));
+            }
+        });
+
+        return v;
+    }
 }
